e85a7f4c12a53901801a210190c8569d6b041df2
suessmeister <josephsuess24@gmail.com>
2024-08-25 00:59:26 -0500

    learning gui part 2

--- test_methods.py
+++ test_methods.py
@@ -113,11 +113,62 @@ def input_box_tkinter_2():
     root.mainloop()
 
 
+def simple_calculator_tkinter_3():
+    root = Tk()
+    root.title("Simple Calculator")
+
+    entry = Entry(root, width=35, borderwidth=5)
+    entry.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
+
+    def button_add(number):
+        current = entry.get() 
+        entry.insert(0, "" + current + number)
+
+    # Define Buttons
+    button_1 = Button(root, text="1", padx=40, pady=20, command=lambda: button_add(1))
+    button_2 = Button(root, text="2", padx=40, pady=20, command=lambda: button_add(2))
+    button_3 = Button(root, text="3", padx=40, pady=20, command=lambda: button_add(3))
+    button_4 = Button(root, text="4", padx=40, pady=20, command=lambda: button_add(4))
+    button_5 = Button(root, text="5", padx=40, pady=20, command=lambda: button_add(5))
+    button_6 = Button(root, text="6", padx=40, pady=20, command=lambda: button_add(6))
+    button_7 = Button(root, text="7", padx=40, pady=20, command=lambda: button_add(7))
+    button_8 = Button(root, text="8", padx=40, pady=20, command=lambda: button_add(8))
+    button_9 = Button(root, text="9", padx=40, pady=20, command=lambda: button_add(9))
+    button_0 = Button(root, text="0", padx=40, pady=20, command=lambda: button_add(0))
+
+    button_adding = Button(root, text="+", padx=40, pady=20, command=button_add)
+    button_equals = Button(root, text="=", padx=40, pady=20, command=button_add)
+    button_clear = Button(root, text="Clear", padx=30, pady=20, command=button_add)
+
+    # Putting buttons on the screen
+    button_1.grid(row=3, column=0)
+    button_2.grid(row=3, column=1)
+    button_3.grid(row=3, column=2)
+
+    button_4.grid(row=2, column=0)
+    button_5.grid(row=2, column=1)
+    button_6.grid(row=2, column=2)
+
+    button_7.grid(row=1, column=0)
+    button_8.grid(row=1, column=1)
+    button_9.grid(row=1, column=2)
+
+    button_0.grid(row=4, column=0)
+    button_adding.grid(row=4, column=1)
+    button_equals.grid(row=4, column=2)
+    button_clear.grid(row=5, column=0)
+
+    entry.insert(0, " ")
+
+    root.mainloop()
+
+
 def driver():
     # frequency_demo_aubio()
     # test_input_devices()
     # click_button_tkinter_1()
-    input_box_tkinter_2()
+    # input_box_tkinter_2()
+    simple_calculator_tkinter_3()
 
 
 driver()
